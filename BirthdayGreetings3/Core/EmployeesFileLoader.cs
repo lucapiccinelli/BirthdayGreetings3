3a8833e8ae0509ba789a69a760bcb5f8c9c54aa5
Lorenzo Bertini <lorenzobertini@fedabo.com>
2021-08-04 10:00:44 +0200

    commit per france

--- BirthdayGreetings3/Core/EmployeesFileLoader.cs
+++ BirthdayGreetings3/Core/EmployeesFileLoader.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using BirthdayGreetings3.Core.Domain.Model;
+using BirthdayGreetings3.Core.Exceptions;
 
 namespace BirthdayGreetings3.Core
 {
@@ -13,10 +14,24 @@ namespace BirthdayGreetings3.Core
         {
             var fileLines = File.ReadAllLines(filename);
             IEnumerable<string> employeesLines = SkipHeader(fileLines);
+            List<EmployeeParsingException> parsingErrors = new List<EmployeeParsingException>();
 
-            return employeesLines
-                .Select(EmployeeParser.ToEmployee)
-                .ToList();
+
+            List<Employee> list = new List<Employee>();
+            foreach (var employeeLine in employeesLines)
+            {
+                try
+                {
+                    Employee employee = EmployeeParser.ToEmployee(employeeLine);
+                    list.Add(employee);
+                }
+                catch (EmployeeParsingException e)
+                {
+                    parsingErrors.Add(e);
+                }
+            }
+            return list;
+
         }
 
         private static IEnumerable<string> SkipHeader(string[] fileLines)
