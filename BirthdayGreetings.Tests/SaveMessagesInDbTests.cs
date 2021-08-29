39d27a3c5b2c82e5cb95562a9334096b0cb90450
Luca Piccinelli <luca.picci@gmail.com>
2021-08-29 08:45:26 +0200

    test get messages by date

--- BirthdayGreetings.Tests/SaveMessagesInDbTests.cs
+++ BirthdayGreetings.Tests/SaveMessagesInDbTests.cs
@@ -13,25 +13,55 @@ namespace BirthdayGreetings.Tests
 {
     public class SaveMessagesInDbTests
     {
+        private readonly Mock<IEmployeesReadSource> _employeesRepo;
+
+        public SaveMessagesInDbTests()
+        {
+            _employeesRepo = new Mock<IEmployeesReadSource>();
+            _employeesRepo
+                .Setup(repository => repository.GetAll())
+                .Returns(() => EmployeesTestsHelper.TestEmployees);
+        }
+
         [Fact]
         public void CanStoreBirthdayMessages()
         {
-            Mock<IEmployeesReadSource> employeesRepo = new Mock<IEmployeesReadSource>();
-            employeesRepo
-                .Setup(repository => repository.GetAll())
-                .Returns(() => EmployeesTestsHelper.TestEmployees);
-
+            var messagesRepository = new InMemoryMessagesRepository();
             StoreBirthdayMessagesService service = new StoreBirthdayMessagesService(
-                employeesRepo.Object, 
-                new InMemoryMessagesRepository());
+                _employeesRepo.Object, 
+                messagesRepository);
 
             var today = EmployeesTestsHelper.John.BirthDate.AddYears(30);
             service.SaveBirthDaysOf(today);
 
-            List<BirthdayMessage> messages = service.GetSavedMessages(today);
+            List<BirthdayMessage> messages = messagesRepository.GetAll();
             List<BirthdayMessage> expectedMessages = new List<BirthdayMessage>
             {
                 new BirthdayMessage(EmployeesTestsHelper.John, today)
+            };
+
+            Assert.Equal(expectedMessages, messages);
+        }
+
+        [Fact]
+        public void CanGetMessagesByDate()
+        {
+            var messagesRepository = new InMemoryMessagesRepository();
+            StoreBirthdayMessagesService service = new StoreBirthdayMessagesService(
+                _employeesRepo.Object, 
+                messagesRepository);
+
+            var today1 = EmployeesTestsHelper.Mary.BirthDate.AddYears(30);
+            service.SaveBirthDaysOf(today1);
+
+            var today2 = EmployeesTestsHelper.John.BirthDate.AddYears(30);
+            service.SaveBirthDaysOf(today2);
+
+
+            List<BirthdayMessage> messages = service.GetSavedMessages(today1);
+            List<BirthdayMessage> expectedMessages = new List<BirthdayMessage>
+            {
+                new BirthdayMessage(EmployeesTestsHelper.Mary, today1)
             };
 
             Assert.Equal(expectedMessages, messages);
