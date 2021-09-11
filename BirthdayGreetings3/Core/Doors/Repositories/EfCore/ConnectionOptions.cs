81cad4462cdad23f44e131c5a09f8acb45bb4803
Luca Piccinelli <luca.picci@gmail.com>
2021-09-11 09:37:06 +0200

    MySql db context

---
+++ BirthdayGreetings3/Core/Doors/Repositories/EfCore/ConnectionOptions.cs
@@ -0,0 +1,25 @@
+﻿namespace BirthdayGreetings3.Core.Doors.Repositories.EfCore
+{
+    public class ConnectionOptions
+    {
+        public string Host { get; }
+        public string Port { get; }
+        public string DbName { get; }
+        public string User { get; }
+        public string Password { get; }
+
+        public ConnectionOptions(string host, string port, string dbName, string user, string password)
+        {
+            Host = host;
+            Port = port;
+            DbName = dbName;
+            User = user;
+            Password = password;
+        }
+
+        public string ConnectionString()
+        {
+            return $"server={Host};port={Port};Database={DbName};Uid={User};Pwd={Password};";
+        }
+    }
+}
---
+++ BirthdayGreetings3/Core/Doors/Repositories/EfCore/MySqlBirthdayDbContext.cs
@@ -0,0 +1,41 @@
+﻿using BirthdayGreetings3.Core.Doors.Repositories.EfCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BirthdayGreetings3.Core.Doors.Repositories.EfCore
+{
+    public class MySqlBirthdayDbContext: DbContext, IBirthdayDbContext
+    {
+        private readonly string _connectionString;
+
+        public MySqlBirthdayDbContext(DbContextOptions options, string connectionString) : base(options)
+        {
+            _connectionString = connectionString;
+        }
+        public MySqlBirthdayDbContext(ConnectionOptions connectionOptions) : base()
+        {
+            _connectionString = connectionOptions.ConnectionString();
+        }
+
+        public DbSet<BirthdayMessageEntity> BithdayMessages { get; set; }
+
+        public void Migrate() => Database.Migrate();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_connectionString);
+        }
+    }
+
+    public class MySqlBirthdayDbContextDesign : IDesignTimeDbContextFactory<MySqlBirthdayDbContext>
+    {
+        public MySqlBirthdayDbContext CreateDbContext(string[] args)
+        {
+            string connectionString = args[0];
+
+            DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder<MySqlBirthdayDbContext>();
+            optionsBuilder.UseMySQL(connectionString);
+            return new MySqlBirthdayDbContext(optionsBuilder.Options, connectionString);
+        }
+    }
+}
