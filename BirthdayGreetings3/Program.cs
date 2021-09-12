4bfc75e6d4e4deac03eaefad5ca3bcce8c08e915
Luca Piccinelli <luca.picci@gmail.com>
2021-09-12 15:12:04 +0200

    main program

--- BirthdayGreetings3/Program.cs
+++ BirthdayGreetings3/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using BirthdayGreetings3.Core;
+using BirthdayGreetings3.Core.Domain.UseCases;
+using BirthdayGreetings3.Core.Doors.Repositories.Csv;
+using BirthdayGreetings3.Core.Doors.Repositories.EfCore;
 
 namespace BirthdayGreetings3
 {
@@ -7,7 +10,13 @@ namespace BirthdayGreetings3
     {
         static void Main(string[] args)
         {
+            StoreBirthdayMessagesService service = new StoreBirthdayMessagesService(
+                new EmployeesCsvReadSource(@"Resources\employees.txt"), 
+                new EfBirthdayMessagesRepository(
+                    new MySqlBirthdayDbContext(
+                        new ConnectionOptions("localhost", 3306, "Test", "root", "sa"))));
 
+            service.SaveBirthDaysOf(DateTime.Now);
         }
     }
 }
