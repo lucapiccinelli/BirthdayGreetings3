2269b1331cdde8d6f39da7a6600f4ea34ce071e3
Luca Piccinelli <luca.picci@gmail.com>
2021-09-12 15:02:48 +0200

    person name

--- BirthdayGreetings3/Core/Domain/Model/Employee.cs
+++ BirthdayGreetings3/Core/Domain/Model/Employee.cs
@@ -63,39 +63,4 @@ namespace BirthdayGreetings3.Core.Domain.Model
             dateOfBirth.Date.Day == today.Date.Day &&
             dateOfBirth.Date.Year <= today.Year;
     }
-
-    public class PersonName
-    {
-        public string Firstname { get; }
-        public string Lastname { get; }
-
-        public PersonName(string firstname, string lastname)
-        {
-            Firstname = firstname;
-            Lastname = lastname;
-        }
-
-        protected bool Equals(PersonName other)
-        {
-            return Firstname == other.Firstname && Lastname == other.Lastname;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((PersonName) obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Firstname, Lastname);
-        }
-
-        public override string ToString()
-        {
-            return $"{nameof(Firstname)}: {Firstname}, {nameof(Lastname)}: {Lastname}";
-        }
-    }
 }
