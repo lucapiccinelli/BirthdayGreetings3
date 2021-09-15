1135d2d86872172624edb498c3ef540dc587e52a
Luca Piccinelli <luca.picci@gmail.com>
2021-09-15 09:58:43 +0200

    store service

--- BirthdayGreetings.Tests/Integration/StoreMessagesInDatabaseTests.cs
+++ BirthdayGreetings.Tests/Integration/StoreMessagesInDatabaseTests.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using BirthdayGreetings.Tests.Unit;
+using BirthdayGreetings3.Core.Domain.Doors;
 using BirthdayGreetings3.Core.Domain.Model;
+using BirthdayGreetings3.Core.Domain.UseCases;
 using BirthdayGreetings3.Core.Doors;
+using Moq;
 using Xunit;
 
 namespace BirthdayGreetings.Tests.Integration
 {
-    class StoreMessagesInDatabaseTests
+    public class StoreMessagesInDatabaseTests
     {
 
-        public void test()
+        [Fact]
+        public void CanStoreMessagesInDb()
         {
-            BirthdayStoreService service = new BirthdayStoreService(EmployeesTestsHelper.TestEmployees);
+            Mock<IEmployeesRepository> repoMock = new Mock<IEmployeesRepository>();
+            repoMock
+                .Setup(repository => repository.GetAll())
+                .Returns(() => EmployeesTestsHelper.TestEmployees);
+
+            var birthdayMessagesRepository = new InMemoryBirthdayMessagesRepository();
+            BirthdayStoreService service = new BirthdayStoreService(repoMock.Object, birthdayMessagesRepository);
 
             var today = EmployeesTestsHelper.John.BirthDate.AddYears(30);
             service.StoreBirthdayMessages(today);
@@ -23,23 +33,47 @@ namespace BirthdayGreetings.Tests.Integration
                 new BirthdayMessage(EmployeesTestsHelper.John.Name, today)
             };
 
-            Assert.Equal(expectedMessages, service._birtdayMessages); 
+            Assert.Equal(expectedMessages, birthdayMessagesRepository.GetAll()); 
         }
 
     }
 
-    class BirthdayStoreService
+    public class BirthdayStoreService
     {
-        public IEnumerable<BirthdayMessage> _birtdayMessages;
+        private readonly BirthdayMessagesService _service;
+        private readonly IRepository<BirthdayMessage> _birthdayMessagesRepository;
 
-        public BirthdayStoreService(List<Employee> testEmployees)
+        public BirthdayStoreService(IEmployeesRepository repository, IRepository<BirthdayMessage> birthdayMessagesRepository)
         {
-            
+            _birthdayMessagesRepository = birthdayMessagesRepository;
+            _service = new BirthdayMessagesService(repository);
         }
 
         public void StoreBirthdayMessages(in DateTime today)
         {
-            
+            _service.CreateMessages(today)
+                .ForEach(message => _birthdayMessagesRepository.Save(message));
+        }
+    }
+
+    public interface IRepository<T>
+    {
+        void Save(T birthdayMessages);
+        IEnumerable<T> GetAll();
+    }
+
+    public class InMemoryBirthdayMessagesRepository : IRepository<BirthdayMessage>
+    {
+        private readonly List<BirthdayMessage> _birtdayMessages = new List<BirthdayMessage>();
+
+        public void Save(BirthdayMessage birthdayMessages)
+        {
+            _birtdayMessages.Add(birthdayMessages);;
+        }
+
+        public IEnumerable<BirthdayMessage> GetAll()
+        {
+            return _birtdayMessages;
         }
     }
 }
